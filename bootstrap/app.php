ec124b5b318c5cb112256f2fd71fa13f147cc3b5
wahyuajisuryantoro <ajiwahyu334@gmail.com>
2025-09-20 01:02:25 +0700

    cors fixing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,9 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->appendToGroup('api', Cors::class);
+        $middleware->prepend(HandleCors::class);
         $middleware->alias([
-
             'api.auth' => ApiAuth::class,
         ]);
     })
